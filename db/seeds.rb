16cd056147ea9e6c476dbea6250adb573b8b1356
Nicholas Restifo <nsrestifo@hotmail.com>
2017-05-03 00:19:31 -0400

    fixed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-stock_history = StockHistory.create(ticker: "test_stock_history")
-
+stock_history = StockHistory.create(ticker: 'test_stock_history')
+stock_history.save
