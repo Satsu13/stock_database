c6294e885e22b2c6ebcd9edd47707984509d12fc
Nicholas Restifo <nsrestifo@hotmail.com>
2017-05-06 13:23:35 -0400

    generated model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :models
   resources :stock_days
   resources :stock_histories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
