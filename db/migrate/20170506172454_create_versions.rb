19b522d0eb211f61d04970ac308fc4f48d7bf555
Nicholas Restifo <nsrestifo@hotmail.com>
2017-05-06 19:38:19 -0400

    change int to integer in migration

--- db/migrate/20170506172454_create_versions.rb
+++ db/migrate/20170506172454_create_versions.rb
@@ -1,7 +1,7 @@
 class CreateVersions < ActiveRecord::Migration[5.0]
   def change
     create_table :versions do |t|
-      t.int :number
+      t.integer :number
       t.references :model, foreign_key: true
 
       t.timestamps
