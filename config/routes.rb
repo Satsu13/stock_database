a772525f49a38a3760981ef7ebdf2d5a033cd8da
Nicholas Restifo <nsrestifo@hotmail.com>
2017-05-08 22:01:06 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  resources :arguments
-  resources :design_axis
-  resources :parameters
-  resources :mature_investments
-  resources :simulations
-  resources :versions
-  resources :models
-  resources :stock_days
-  resources :stock_histories
+  jsonapi_resources :arguments
+  jsonapi_resources :design_axis
+  jsonapi_resources :parameters
+  jsonapi_resources :mature_investments
+  jsonapi_resources :simulations
+  jsonapi_resources :versions
+  jsonapi_resources :models
+  jsonapi_resources :stock_days
+  jsonapi_resources :stock_histories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
