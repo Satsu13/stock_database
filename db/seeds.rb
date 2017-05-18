8c48df1c0216ea8f94c41cf1974605d8273b2170
Nicholas Restifo <nsrestifo@hotmail.com>
2017-05-17 21:39:49 -0400

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-history = StockHistory.create(ticker: 'test_history')
+history = StockHistory.create(ticker: 'test_history_1')
 StockDay.create(date: DateTime.new(2015, 6, 22), value: 5.0, stock_history: history)
 StockDay.create(date: DateTime.new(2015, 6, 23), value: 4.0, stock_history: history)
+
+history = StockHistory.create(ticker: 'test_history_2')
+StockDay.create(date: DateTime.new(2015, 6, 24), value: 3.0, stock_history: history)
+StockDay.create(date: DateTime.new(2015, 6, 25), value: 2.0, stock_history: history)
